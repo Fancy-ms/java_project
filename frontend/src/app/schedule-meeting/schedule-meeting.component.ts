c4109c8991ef52441df09f16dc65546b35aca290
Fancy-ms <ms85949866@gmail.com>
2026-05-04 14:59:44 +0530

    Update schedule-meeting.component.ts

--- frontend/src/app/schedule-meeting/schedule-meeting.component.ts
+++ frontend/src/app/schedule-meeting/schedule-meeting.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MeetingService } from '../services/meeting.service';
+import { MeetingService } from '../../services/meeting.service';
 
 @Component({
   selector: 'app-schedule-meeting',
